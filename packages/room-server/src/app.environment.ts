6fe6ea1e6b00be1a66e87244416894b6e97cda81
亮亮 <61300727+LiangLiang723@users.noreply.github.com>
2026-05-24 13:06:24 +0800

    feat: add self-hosted enterprise room flags

--- packages/room-server/src/app.environment.ts
+++ packages/room-server/src/app.environment.ts
@@ -42,8 +42,9 @@ export const PROJECT_DIR = __dirname;
 export const defaultLanguage = process.env.DEFAULT_LANGUAGE || 'en-US';
 
 export const supportedLanguages = process.env.SUPPORTED_LANGUAGES ? process.env.SUPPORTED_LANGUAGES.split(',') : ['zh-CN', 'en-US'];
-export const skipUsageVerification = Object.is(process.env.SKIP_USAGE_VERIFICATION, 'true');
-export const skipApiUsageVerification = Object.is(process.env.SKIP_API_USAGE_VERIFICATION, 'true');
+export const selfHostedEnterprise = Object.is(process.env.SELF_HOSTED_ENTERPRISE, 'true');
+export const skipUsageVerification = selfHostedEnterprise || Object.is(process.env.SKIP_USAGE_VERIFICATION, 'true');
+export const skipApiUsageVerification = selfHostedEnterprise || Object.is(process.env.SKIP_API_USAGE_VERIFICATION, 'true');
 /**
  * whether show anonymous person in room.
  */
